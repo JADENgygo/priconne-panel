00db5b0a797fd6e72581ff87cf13c95f7e45fbc7
JADEN <29948746+JADENgygo@users.noreply.github.com>
2022-08-03 19:51:17 +0900

    カメラ切替え時に元画像と反転画像の両方が描画されるバグを修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,57 +1,88 @@
 import type { NextPage } from 'next'
-import { useState, useEffect } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 const Home: NextPage = () => {
   const [faced, setFaced] = useState(true);
-  let cameraStream: MediaStream | null = null;
+
+  const [image, setImage] = useState((null as unknown) as HTMLImageElement);
+  useEffect(() => {
+    const img = document.createElement('img')
+    img.src = "/panel.png";
+    setImage(img);
+  }, []);
+
+  const [cameraStream, setCameraStream] = useState((null as unknown) as MediaStream);
   const changeCamera = () => {
     if (cameraStream !== null) {
       cameraStream.getVideoTracks().forEach(e => e.stop());
     }
     setFaced(!faced);
   }
+
+  const callback = (streamWidth: number, streamHeight: number) => {
+    const video = document.getElementById('video') as HTMLVideoElement;
+    if (!video) {
+      return;
+    }
+    const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+    if (!canvas) {
+      return;
+    }
+    const context = canvas.getContext('2d');
+    if (!context) {
+      return;
+    }
+
+    // const img = document.createElement('img');
+    // img.src = "/panel.png";
+
+    if (faced) {
+      context.scale(-1, 1);
+      context.translate(-canvas.width, 0);
+    }
+    context.drawImage(video, canvas.width / 2 - streamWidth / 2, canvas.height / 2 - streamHeight / 2);
+
+    if (faced) {
+      context.scale(-1, 1);
+      context.translate(-canvas.width, 0);
+    }
+    context.drawImage(image, 0, 0);
+  };
+
+  const callbackRef = useRef(callback);
+  useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+
   useEffect(() => {
     const video = document.getElementById('video') as HTMLVideoElement;
     if (!video) {
       return;
     }
+
+    let intervalId: number;
     const config = { video: { facingMode: faced ? "user" : {exact: 'environment'} }, audio: false };
     navigator.mediaDevices.getUserMedia(config).then(stream => {
+      alert("tracks: " + stream.getVideoTracks().length)
+
       video.srcObject = stream;
-      cameraStream = stream;
-
-      const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-      if (!canvas) {
+      setCameraStream(_ => stream);
+      const streamWidth = stream.getVideoTracks()[0].getSettings().width;
+      const streamHeight = stream.getVideoTracks()[0].getSettings().height;
+      if (!streamWidth) {
         return;
       }
-      const context = canvas.getContext('2d');
-      if (!context) {
+      if (!streamHeight) {
         return;
       }
-
-      const img = document.createElement('img');
-      img.src = "/panel.png";
-
-      setInterval(() => {
-        // if (faced) {
-        //   context.scale(-1, 1);
-        //   context.translate(-canvas.width, 0);
-        // }
-
-        context.drawImage(video, 0, 0);
-
-        // if (faced) {
-        //   context.scale(-1, 1);
-        //   context.translate(-canvas.width, 0);
-        // }
-
-        //context.drawImage(img, 0, 0);
-      }, 33);
+      intervalId = window.setInterval(() => callbackRef.current(streamWidth, streamHeight), 33);
     }).catch(err => console.log('error: ' + err));
+    return () => clearInterval(intervalId);
   }, [faced]);
 
   const width = 727;
   const height = 637;
+
   const saveImage = () => {
     const canvas = document.getElementById("canvas") as HTMLCanvasElement;
     if (!canvas) {
@@ -62,6 +93,7 @@ const Home: NextPage = () => {
     a.download = 'panel.png';
     a.click();
   };
+
   return (
     <div className="container pt-3">
       <div className="text-center">
@@ -70,7 +102,7 @@ const Home: NextPage = () => {
         <p>元ネタ: <a href="https://cystore.com/products/4573478717023">エリザベスパークの特大顔出しパネル</a></p>
         <canvas id="canvas" width={width} height={height} />
         <div>
-          <video id="video" width={1} height={1} playsInline autoPlay muted loop style={{"transform": faced ? "scaleX(-1)" : "scaleX(1)"}} />
+          <video id="video" width={1} height={1} playsInline autoPlay muted loop style={{"transform": faced ? "scaleX(1)" : "scaleX(1)"}} />
         </div>
         <div className="mb-3">
           <button onClick={changeCamera} className="btn btn-dark me-3">カメラ切替</button>
